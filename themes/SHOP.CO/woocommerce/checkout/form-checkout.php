ce57417bb07acdccd2827b9b1589d09c72a29519
filipd1 <filip.dudziak2@o2.pl>
2026-03-24 12:08:25 +0100

    checkout fixed and styling improved

--- themes/SHOP.CO/woocommerce/checkout/form-checkout.php
+++ themes/SHOP.CO/woocommerce/checkout/form-checkout.php
@@ -1,28 +1,40 @@
 <?php
-	defined('ABSPATH') || exit;
+    defined('ABSPATH') || exit;
+    do_action( 'woocommerce_before_checkout_form', $checkout );
+
+    if ( ! $checkout->is_registration_enabled() && $checkout->is_registration_required() && ! is_user_logged_in() ) {
+        echo esc_html( apply_filters( 'woocommerce_checkout_must_be_logged_in_message', __( 'You must be logged in to checkout.', 'woocommerce' ) ) );
+        return;
+    }
 ?>
 
 <div class="checkout-container">
+    <h1>Checkout</h1>
 
-	<h1>Checkout</h1>
+    <form name="checkout" method="post" class="checkout woocommerce-checkout" action="<?php echo esc_url( wc_get_checkout_url() ); ?>" enctype="multipart/form-data">
 
-	<form class="checkout" method="post">
+        <div class="checkout-grid">
 
-		<div class="checkout-grid">
+            <div class="checkout-left">
+                <?php do_action('woocommerce_checkout_billing'); ?>
+                
+                <h2>Shipping details</h2>
+                <?php do_action('woocommerce_checkout_shipping'); ?>
+            </div>
 
-			<div class="checkout-left">
-				<?php do_action('woocommerce_checkout_billing'); ?>
-				<h2>Shipping details</h2>
-				<?php do_action('woocommerce_checkout_shipping'); ?>
-			</div>
+            <div class="checkout-right">
+                <h2>Your order</h2>
+                
+                <div id="order_review" class="woocommerce-checkout-review-order">
+                    <?php do_action('woocommerce_checkout_order_review'); ?>
+                </div>
+            </div>
 
-			<div class="checkout-right">
-				<h2>Your order</h2>
-				<?php do_action('woocommerce_checkout_order_review'); ?>
-			</div>
+        </div>
 
-		</div>
-
-	</form>
-
+    </form>
 </div>
+
+<?php 
+    do_action( 'woocommerce_after_checkout_form', $checkout ); 
+?>
